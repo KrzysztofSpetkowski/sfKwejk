bcdd5228784c0b1767f3644932cb2ff5b9de7e15
krzysiek <k.spetkowski@wp.pl>
2014-11-07 20:39:03 +0100

    wyswietlanie memow cz1

--- src/Kwejk/MemsBundle/Entity/Mem.php
+++ src/Kwejk/MemsBundle/Entity/Mem.php
@@ -181,6 +181,7 @@ class Mem
      */
     public function __construct()
     {
+    	$this->createdAt = new \DateTime('now');
         $this->comments = new \Doctrine\Common\Collections\ArrayCollection();
         $this->ratings = new \Doctrine\Common\Collections\ArrayCollection();
     }

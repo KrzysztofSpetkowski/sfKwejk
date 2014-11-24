f9674b33f8e5370e86c2211063d7163319436f8d
krzysiek <k.spetkowski@wp.pl>
2014-11-25 00:15:19 +0100

    change menu

--- src/Kwejk/LayoutBundle/Menu/Builder.php
+++ src/Kwejk/LayoutBundle/Menu/Builder.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Kwejk\LayoutBundle\Menu;
+
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
@@ -9,6 +11,7 @@ class Builder extends ContainerAware
     {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        
         $menu->addChild('Strona główna', [
             'route' => 'kwejk_mems_list'
         ]);
@@ -21,14 +24,36 @@ class Builder extends ContainerAware
         $menu->addChild('Losuj', [
             'uri' => '#'
         ]);
+         return $menu;
         
-        /*
-        $menu->addChild('User')
-            ->setAttribute('dropdown', true);
-        $menu['User']->addChild('Profile', array('uri' => '#'))
+    }
+        
+    public function userMenuAuthenticated(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+    
+        $menu->addChild('User', ['uri' => '#'])
+            ->setAttribute('class', 'dropdown')
+            ->setLinkAttribute('class', 'dropdown-toggle')
+            ->setLinkAttribute('data-toggle', 'dropdown')
+            ->setChildrenAttribute('class', 'dropdown-menu');
+        $menu['User']->addChild('Profil', array('route' => 'fos_user_profile_edit'))
             ->setAttribute('divider_append', true);
-        $menu['User']->addChild('Logout', array('uri' => '#'));
-        */
+        $menu['User']->addChild('Wyloguj', array('route' => 'fos_user_security_logout'));
+    
+        return $menu;
+    }
+    
+    public function userMenuNotAuthenticated(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+    
+        $menu->addChild('Zaloguj', [
+            'route' => 'fos_user_security_login'
+        ]);
+    
         return $menu;
     }
 }

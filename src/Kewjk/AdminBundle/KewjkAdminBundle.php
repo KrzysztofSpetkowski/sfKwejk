96ab52b3a7788605eb0b89171504cbc382a3e53b
krzysiek <k.spetkowski@wp.pl>
2014-11-25 21:37:19 +0100

    sample test added

--- src/Kewjk/AdminBundle/KewjkAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Kewjk\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class KewjkAdminBundle extends Bundle
-{
-}
---
+++ src/Kwejk/MemsBundle/Tests/Controller/MemsControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Mems\MemsBundle\Tests\Controller;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+class MemsControllerTest extends WebTestCase
+{
+    public function testList()
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+        
+        $crawler = $client->request('GET', '/');
+        
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Strona główna")')->count()
+        );
+        
+        $link = $crawler->filter('a:contains("Dodaj mema")')->eq(0)->link();
+        
+        $crawler = $client->click($link);
+        
+        // Panel logowania
+        $this->assertEquals(
+            1,
+            $crawler->filter('html:contains("Panel logowania")')->count()
+        );
+        
+    }
+    public function testShow()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/{slug}');
+    }
+}
